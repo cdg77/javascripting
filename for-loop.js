113f8ac5455b2be33e258cac6cbd3e7ff2f80499
cdg77 <cdegregorio@gmail.com>
2014-10-20 12:40:48 -0700

    for-loop challenge complete

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+var total = 0;
+var limit = 10;
+
+for (i=0; i<=10; i++) {
+	console.log('i = :' + i);
+	total += i;
+	console.log('total inside = :' + total);
+}
+
+	console.log('total outside = :' + total);
+
+	
